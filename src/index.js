c5210b0cae7b8465ba51c450bc27551791f4e570
YazidZidane <xjt980408@gmail.com>
2024-09-06 14:05:10 -0400

    add ncalc map

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ export { default as RegionSelectorMap } from "./region-selector-map/index";
 export { default as Spinner } from "./spinner/index";
 export { default as Map } from "./map/index";
 export { default as FadeAlert } from "./fade-alert/index";
+export { default as NcalcMap } from "./ncalc-map/index";
