a097674cef96333ff6c4d621d8b156c938ed3ac3
YazidZidane <xjt980408@gmail.com>
2024-10-07 12:17:58 -0400

    resolve console err

--- src/Textfield/textfield.jsx
+++ src/Textfield/textfield.jsx
@@ -3,4 +3,4 @@ import { TextField, styled } from "@mui/material";
 /**
  * This component is based on [MUI TextField](https://mui.com/material-ui/react-text-field/) component.
  */
-export const PSATextField = styled(TextField)();
+export const PSATextField = styled(TextField)({});
