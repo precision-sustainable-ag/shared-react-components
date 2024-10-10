dcf81df0f2a5ccec49963cedecf3ee99649b24dd
YazidZidane <xjt980408@gmail.com>
2024-10-10 16:03:13 -0400

    create theme

---
+++ src/theme/index.js
@@ -0,0 +1,26 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    main: {
+      accent1: "#598445",
+      accent2: "#27739E",
+      background1: "#E9E6E0",
+      text: "#1F1F1F",
+    },
+    additional: {
+      background2: "#F5F5F5",
+      grey2: "#AAAAAA",
+      grey1: "#737373",
+      greydark: "#565656",
+      support1: "#416782",
+      support2: "#408D79",
+      support3: "#C48B0F",
+      support4: "#91643B",
+      support5: "#624469",
+      error: "#416782",
+    },
+  },
+});
+
+export default theme;
