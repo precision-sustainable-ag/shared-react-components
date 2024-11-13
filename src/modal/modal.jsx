61971a1b87a05b032e829796847237dbb04f7d43
Diksha Maurya <mauryadiksha0@gmail.com>
2024-11-13 13:56:24 -0500

    ...props implemented

--- src/modal/modal.jsx
+++ src/modal/modal.jsx
@@ -3,22 +3,11 @@ import PropTypes from "prop-types";
 import { Modal } from '@mui/material';
 
 export function PSAModal({
-  modalContent, 
-  sx, 
-  style, 
-  open, 
-  onClose, 
-  closeAfterTransition, 
-  disableEscapeKeyDown
+  modalContent, ...props
 }) {
   return (
     <Modal
-      sx={sx}
-      open={open}
-      style={style}
-      onClose={onClose}
-      closeAfterTransition={closeAfterTransition}
-      disableEscapeKeyDown={disableEscapeKeyDown}
+      {...props}
     >
     {modalContent}
   </Modal>
@@ -32,34 +21,4 @@ PSAModal.propTypes = {
    * The content of the modal.
    */
   modalContent: PropTypes.node.isRequired,
-
-  /**
-   * The system prop that allows defining system overrides as well as additional CSS styles.
-   */
-  sx: PropTypes.object,
-
-  /**
-   * React styling object to apply custom styles.
-   */
-  style: PropTypes.object,
-
-  /**
-   * If `true`, the modal is open.
-   */
-  open: PropTypes.bool.isRequired,
-
-  /**
-   * Callback fired when the component requests to be closed.
-   */
-  onClose: PropTypes.func,
-
-  /**
-   * If `true`, the modal will close after the transition is completed.
-   */
-  closeAfterTransition: PropTypes.bool,
-
-  /**
-   * If `true`, hitting the Escape key will not fire the `onClose` callback.
-   */
-  disableEscapeKeyDown: PropTypes.bool,
 };
