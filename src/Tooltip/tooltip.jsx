3f48ab9fa0cfaed5d10efbb44959b9892b7ea0f7
Diksha Maurya <mauryadiksha0@gmail.com>
2024-12-06 17:52:02 -0500

    onmouseenter and onmouseleave removed

--- src/Tooltip/tooltip.jsx
+++ src/Tooltip/tooltip.jsx
@@ -3,20 +3,13 @@ import PropTypes from "prop-types";
 import { Tooltip } from '@mui/material';
 
 export function PSATooltip({
-  onMouseEnter,//Extracting onMouseEnter from the props.
-  onMouseLeave,//Extracting onMouseLeave from the props.
   tooltipContent, 
   ...props
 }) {
-  // Cloning tooltipContent and adding/injecting event handlers directly to element
-  const eventHandlers = {};
-  if (onMouseEnter) eventHandlers.onMouseEnter = onMouseEnter;
-  if (onMouseLeave) eventHandlers.onMouseLeave = onMouseLeave;
-  const clonedTooltipContent = React.cloneElement(tooltipContent, eventHandlers);
   return (
     <Tooltip
       {...props}
-      children={clonedTooltipContent}
+      children={tooltipContent}
     />
   );
 }
@@ -27,15 +20,4 @@ PSATooltip.propTypes = {
    * Custom content to render inside the tooltip.
    */
   tooltipContent: PropTypes.node,
-
-  /**
-   * Mouse enter event handler.
-   */
-  onMouseEnter: PropTypes.func,
-
-  /**
-   * Mouse leave event handler.
-   */
-  onMouseLeave: PropTypes.func,
-
 };
