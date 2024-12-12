562dcf1a1e0f7ae6d6f8ec308d2ff35c0786e12e
Ravi Ghevariya <ravighevariya10@gmail.com>
2024-12-12 13:34:41 -0500

    1. Added the propTypes 2. Updated the logic for steps accessibility

--- src/Progressstepper/progressstepper.jsx
+++ src/Progressstepper/progressstepper.jsx
@@ -1,22 +1,23 @@
 import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 import { Box, Step, StepButton, Stepper, styled, Typography } from '@mui/material';
 import StepConnector, { stepConnectorClasses } from '@mui/material/StepConnector';
 
-const StepLight = () => (
+const StepLight = ({ strokeColor }) => (
   <svg xmlns="http://www.w3.org/2000/svg" width="43" height="43" viewBox="0 0 43 43" fill="none">
-    <circle cx="21.4318" cy="21.4318" r="17.9318" fill="#AAAAAA" stroke="#F5F5F5" strokeWidth="7" />
+    <circle cx="21.4318" cy="21.4318" r="17.9318" fill="#AAAAAA" stroke={strokeColor} strokeWidth="7" />
   </svg>
 );
 
-const StepDark = () => (
+const StepDark = ({ strokeColor }) => (
   <svg xmlns="http://www.w3.org/2000/svg" width="43" height="43" viewBox="0 0 43 43" fill="none">
-    <circle cx="21.5001" cy="21.4318" r="17.9318" fill="#363636" stroke="#F5F5F5" strokeWidth="7" />
+    <circle cx="21.5001" cy="21.4318" r="17.9318" fill="#363636" stroke={strokeColor} strokeWidth="7" />
   </svg>
 );
 
-const StepActive = () => (
+const StepActive = ({ strokeColor }) => (
   <svg xmlns="http://www.w3.org/2000/svg" width="42" height="42" viewBox="0 0 42 42" fill="none">
-    <circle cx="21" cy="21" r="20" fill="#334A03" stroke="#F5F5F5" strokeWidth="17" />
+    <circle cx="21" cy="21" r="20" fill="#334A03" stroke={strokeColor} strokeWidth="17" />
     <circle cx="21" cy="21" r="19" stroke="#334A03" strokeWidth="3" />
   </svg>
 );
@@ -47,13 +48,14 @@ const CustomStepConnector = styled(StepConnector)(() => ({
 export const PSAProgressstepper = ({
   steps = [],
   tabs = [],
-  maxAvailableStep = 1,
+  maxAvailableStep,
   onStepClick = () => { },
   boxProps = {},
   stepperProps = {},
   stepProps = {},
   stepButtonProps = {},
   typographyProps = {},
+  strokeColor = "#F5F5F5",
 }) => {
 
   const [activeStep, setActiveStep] = useState(0);
@@ -71,18 +73,32 @@ export const PSAProgressstepper = ({
 
   const getStepIcon = (currStep, activeStep) => {
 
-    const baseIcon = (() => {
-      if (activeStep === currStep) return <StepActive />;
-      if (maxAvailableStep < currStep) return <StepLight />;
-      if (maxAvailableStep >= currStep) return <StepDark />;
-    })();
-  
-    const getTextColor = () => {
-      if (maxAvailableStep === currStep) return 'white';
-      if (maxAvailableStep > currStep) return 'white';  
-      return 'black';                             
-    };
-  
+    const baseIcon = maxAvailableStep != undefined ?
+
+      (() => {
+        if (activeStep === currStep) return <StepActive strokeColor={strokeColor} />;
+        if (maxAvailableStep < currStep) return <StepLight strokeColor={strokeColor} />;
+        if (maxAvailableStep >= currStep) return <StepDark strokeColor={strokeColor} />;
+      })()
+      :
+      (() => {
+        if (activeStep < currStep) return <StepLight strokeColor={strokeColor} />;
+        if (activeStep > currStep) return <StepDark strokeColor={strokeColor} />;
+        else return <StepActive strokeColor={strokeColor} />;
+      })()
+
+    const getTextColor = maxAvailableStep != undefined ? 
+    () => {
+      if (activeStep === currStep) return 'white';
+      if (maxAvailableStep >= currStep) return 'white';
+      return 'black';
+    }
+    :
+    () => {
+      if (activeStep >= currStep) return 'white';
+      return 'black';
+    }
+
     return (
       <Box position="relative" display="inline-flex">
         {baseIcon}
@@ -94,7 +110,7 @@ export const PSAProgressstepper = ({
           sx={{
             transform: 'translate(-50%, -50%)',
             color: getTextColor(),
-            fontWeight: maxAvailableStep === currStep ? 'bold' : 'normal',
+            fontWeight: activeStep === currStep ? 'bold' : 'normal',
           }}
         >
           {currStep + 1}
@@ -121,7 +137,7 @@ export const PSAProgressstepper = ({
         {...stepperProps}
       >
         {steps.map((step, index) => (
-          <Step key={index} completed={index < activeStep} disabled={maxAvailableStep < index} {...stepProps}>
+          <Step key={index} completed={index < activeStep} disabled={maxAvailableStep !== undefined ? maxAvailableStep < index : false} {...stepProps}>
             <StepButton
               onClick={() => handleStepClick(tabs[index], index)}
               icon={getStepIcon(index, activeStep)}
@@ -155,3 +171,58 @@ export const PSAProgressstepper = ({
     </Box>
   );
 };
+
+PSAProgressstepper.propTypes = {
+  /**
+   * Array of step labels to display in the stepper.
+   */
+  steps: PropTypes.arrayOf(PropTypes.string).isRequired,
+
+  /**
+   * Array of tabs or additional data that is associated with each step.
+   */
+  tabs: PropTypes.arrayOf(PropTypes.any).isRequired,
+
+  /**
+   * The maximum available step that the user can access (optional).
+   */
+  maxAvailableStep: PropTypes.number,
+
+  /**
+   * Callback function triggered when a step is clicked, receives the tab and index as arguments.
+   */
+  onStepClick: PropTypes.func,
+
+  /**
+   * Additional props for styling the outer box container.
+   */
+  boxProps: PropTypes.shape({
+    sx: PropTypes.object,
+  }),
+
+  /**
+   * Additional props for the Stepper component.
+   */
+  stepperProps: PropTypes.object,
+
+  /**
+   * Additional props for the Step component.
+   */
+  stepProps: PropTypes.object,
+
+  /**
+   * Additional props for the StepButton component.
+   */
+  stepButtonProps: PropTypes.object,
+
+  /**
+   * Additional props for the Typography component inside the StepButton.
+   */
+  typographyProps: PropTypes.object,
+
+  /**
+   * The stroke color for the step icons.
+   */
+  strokeColor: PropTypes.string,
+};
+
--- src/Progressstepper/progressstepper.stories.jsx
+++ src/Progressstepper/progressstepper.stories.jsx
@@ -17,9 +17,24 @@ const Template = (args) => {
   );
 };
 
-export const Default = Template.bind({});
+export const NCalc = Template.bind({});
 
-Default.args = {
+NCalc.args = {
+    steps: [
+      'Home', 'Location', 'Soil', 'Cover Crop', 'Cash Crop', 'Output'
+    ],
+    stepperProps: {
+      activeStep: 0
+    },
+    boxProps: {
+      sx: { backgroundColor: '#F5F5F5', opacity: 0.9, width: '100%' }
+    },
+  };
+
+export const SeedCalc = Template.bind({});
+
+SeedCalc.args = {
+  maxAvailableStep: 2,
     steps: [
       'Home', 'Location', 'Soil', 'Cover Crop', 'Cash Crop', 'Output'
     ],
