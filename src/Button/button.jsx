129af3e4a7eedc95e2ef07cad8911ab85faec5cf
Jingtong Xu <xjt980408@gmail.com>
2024-12-17 09:34:45 -0500

    resolve transparent console warning

--- src/Button/button.jsx
+++ src/Button/button.jsx
@@ -15,7 +15,7 @@ const pillBackgroundColor = (selected, transparent) => {
 
 // Use MUI's styled system to define the button styles
 const StyledButton = styled(Button, {
-  shouldForwardProp: (prop) => prop !== "buttonType",
+  shouldForwardProp: (prop) => prop !== "buttonType" && prop !== "transparent",
 })(({ buttonType, selected, transparent }) => ({
   ...(buttonType === "LightButton" && {
     backgroundColor: "#e3f2f4",
@@ -74,8 +74,8 @@ const StyledButton = styled(Button, {
 }));
 
 export const PSAButton = ({
-  title = "",
-  buttonType = "LightButton",
+  title,
+  buttonType = "",
   hoverText,
   selected = false,
   transparent = false,
