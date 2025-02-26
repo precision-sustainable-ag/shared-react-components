3a85ed1bdfe27b36bc11d7a360b7032657f957b7
Jingtong Xu <xjt980408@gmail.com>
2025-02-26 14:56:47 -0500

    update highchart export module

--- utils/highchartsConfig.js
+++ utils/highchartsConfig.js
@@ -1,7 +1,11 @@
 import Highcharts from "highcharts";
 import Accessibility from "highcharts/modules/accessibility";
+import Exporting from "highcharts/modules/exporting";
+import ExportData from "highcharts/modules/export-data";
 
 // Config for highcharts accessibility
 Accessibility(Highcharts);
+Exporting(Highcharts);
+ExportData(Highcharts);
 
 export default Highcharts;
