9c7e2ecf1a4ca14a16d416d39e16fba0a948456e
Jingtong Xu <xjt980408@gmail.com>
2025-03-17 11:08:34 -0400

    add additional styles to profile

--- src/Profile/profile.jsx
+++ src/Profile/profile.jsx
@@ -1,44 +1,54 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { useAuth0 } from "@auth0/auth0-react";
 import { Box } from "@mui/material";
 
-export function PSAProfile({}) {
+export function PSAProfile({ styles }) {
   const { user, isAuthenticated } = useAuth0();
 
   return (
-    <Box sx={{ border: 0.5, borderColor: 'grey.300' }} ml={2} mr={2} mt={5}>
+    <Box
+      sx={{ border: 0.5, borderColor: "grey.300", ...styles }}
+      ml={2}
+      mr={2}
+      mt={5}
+    >
       {isAuthenticated ? (
-        <div style={{
-          display: 'grid',
-          gridTemplateColumns: 'auto 1fr',
-          columnGap: '16px',
-        }}
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "auto 1fr",
+            columnGap: "16px",
+          }}
         >
           <img
             src={user.picture}
             alt="Profile"
             style={{
-              borderRadius: '50%',
-              height: '80px',
-              width: '80px',
+              borderRadius: "50%",
+              height: "80px",
+              width: "80px",
             }}
           />
-          <div style={{
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'space-around',
-          }}
-          >
-            <h2 style={{
-              marginTop: 0,
-              marginBottom: 0,
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "space-around",
             }}
+          >
+            <h2
+              style={{
+                marginTop: 0,
+                marginBottom: 0,
+              }}
             >
               {user.name}
             </h2>
-            <span style={{
-              fontSize: '1.3rem',
-            }}
+            <span
+              style={{
+                fontSize: "1.3rem",
+              }}
             >
               {user.email}
             </span>
@@ -48,7 +58,12 @@ export function PSAProfile({}) {
         <div>You have not been logged in!</div>
       )}
     </Box>
-
   );
 }
 
+/* Define Props Type */
+
+PSAProfile.propTypes = {
+  /** Additional styles for profile */
+  styles: PropTypes.object,
+};
