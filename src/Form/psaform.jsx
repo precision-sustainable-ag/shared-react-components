58a9143aa62bc855e52276b0761b1671933f9214
Ravi Ghevariya <ravighevariya10@gmail.com>
2026-03-17 15:39:21 -0400

    added repository name as label in the feedback form

--- src/Form/psaform.jsx
+++ src/Form/psaform.jsx
@@ -126,6 +126,7 @@ export const PSAForm = ({
     const payload = { ...formData };
 
     payload.labels = payload.labels || [];
+    payload.labels.push(`${formData.repository}`);
     if (formData.state) {
       payload.labels.push(`State: ${formData.state}`);
     }
